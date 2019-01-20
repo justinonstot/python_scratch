85bde00226932983e2c6525e887e4736856ac4bd
Justin Onstot <justinmonstot@gmail.com>
2019-01-20 11:00:26 -0800

    added a dictionary

--- test.py
+++ test.py
@@ -1,2 +1,8 @@
-print("Hello World!")
-print("This is again")
+user_0 = {
+    'username':'justino',
+    'firstname': 'Justin',
+    'lastname': 'Onstot',}
+
+for key, value in user_0.items():
+    print("\nThe key is: " + key)
+    print('The value is: ' + value)
